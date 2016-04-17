74bfbf586d74bbc9e79afc330df2aa6f7dfe2508
hapekituarangi <hape.porter@gmail.com>
2016-04-17 19:39:33 +1200

    added square root function

--- calculator.js
+++ calculator.js
@@ -1,26 +1,47 @@
 var key = document.querySelectorAll(".wrapper td"); //Select all table cells for calculator buttons
-var result;
-var findResult;
+
+var result;// for finding reuslt calculation 
+
 	for(var i = 0; i < key.length; i++) {
-		key[i].onclick = function(e) {
+		key[i].onclick = function(e) {//add onclick to all td
 			console.log(e);
-			var input = document.querySelector(".display td"); //select table row, make calculator data display area
-			var inputVal = input.innerHTML; //fetch button input data
-			var printInput = this.innerHTML; //print button input data in calculator display area
-			console.log(printInput); //print button input data to console log for testing purposes
+			var input = document.querySelector(".display td");//get the input
+			var inputVal = input.innerHTML;
+			var printInput = this.innerHTML;
+			console.log(printInput);
 
 
 			
-			if(printInput === "CE"){ //if "CE" button clicked, remove last character added to display area , or first character to the right of display area
+			if(printInput === "CE"){// if press CE take the last number form screen
 				input.innerHTML = input.innerHTML.slice(0, -1);
-				$(".textbox");
-			}else if (printInput === "AC") { //if "AC" button clicked, clear all data in display area
+				
+			}else if (printInput === "AC") {// if press AC erase everything
 				input.innerHTML = "";
-			}else if (printInput === "="){ //if "=" button clicked, solve equation, clear current displayed data then replace with solved data
-				result = input.innerHTML;
-				input.innerHTML = eval(result);
-			}else{
-			input.innerHTML += printInput; }
-		}
-	}
+			}else if (printInput === "="){//if press = caculate and display
+				result = inputVal;
+				input.innerHTML = myEval(result);
+			
+       			;
+        		}else{
+				input.innerHTML += printInput;
+      		}
+			function myEval(inputValue)// for calculate square(x2)
+			{//
+				var n = inputValue.indexOf("√")//Find index of power
+				//If n == -1 it means the opoerator is something else so the Eval function will take care of it in Else block.
+				if (n > -1)
+				{
+					var number1 = inputValue.substring(0,n);
+					var number2 =  inputValue.substring(n+1, inputValue.length);
+					return Math.pow(number1, number2);
+				}
+			else
+			{
+			return eval(inputValue);
+			}
+			}
+
+
+					}
+				}
 
